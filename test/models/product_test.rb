ee643da3665d23dda82e0b2c993a1b6e53cb8843
DavidLineman <dline215@gmail.com>
2020-01-16 14:36:28 -0500

    added more validation tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -37,20 +37,30 @@ class ProductTest < ActiveSupport::TestCase
                 image_url:    image_url)
   end
 
-  test "image_url" do
+  test "image url" do
     ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
-              http://a.b.c/x/z/fred.gif }
+              http://a.b.c/x/y/z/fred.gif }
     bad = %w{ fred.doc fred.gif/more fred.gif.more }
 
     ok.each do |image_url|
-      assert new_product(image_url).valid?
+      assert new_product(image_url).valid?,
               "#{image_url} shouldn't be invalid"
     end
 
     bad.each do |image_url|
-      assert new_product(image_url).invalid?
+      assert new_product(image_url).invalid?,
               "#{image_url} shouldn't be valid"
     end
+  end
+
+  test "product is not valid without a unique title - i18n" do
+    product = Product.new(title:        products(:ruby).title,
+                          description:  "yyy",
+                          price:        1,
+                          image_url:    "fred.gif")
+    assert product.invalid?
+    assert_equal [I18n.translate('errors.messages.taken')],
+                  product.errors[:title]
   end
 
 
