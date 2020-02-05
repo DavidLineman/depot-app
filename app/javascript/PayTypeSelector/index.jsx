cd459db92831bad3ff33204b37fe3a9d069776f1
DavidLineman <dline215@gmail.com>
2020-02-05 14:49:35 -0500

    finished pay selector type component

--- app/javascript/PayTypeSelector/index.jsx
+++ app/javascript/PayTypeSelector/index.jsx
@@ -8,8 +8,11 @@ class PayTypeSelector extends React.Component {
         <select id="order_pay_type" name="order[pay_type]">
           <option value="">Select a payment method</option>
           <option value="Check">Check</option>
+          <option value="Credit Card">Credit Card</option>
+          <option value="Purchase order">Purchase order</option>
         </select>
       </div>
-    )
+    );
   }
 }
+export default PayTypeSelector
