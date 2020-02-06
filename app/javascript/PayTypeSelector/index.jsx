4d0e6799788ee30af60c4c939acf995200dae987
DavidLineman <dline215@gmail.com>
2020-02-06 16:44:22 -0500

    credit card payment type react component will nto render, I tinkered with the fix to no avail

--- app/javascript/PayTypeSelector/index.jsx
+++ app/javascript/PayTypeSelector/index.jsx
@@ -30,7 +30,7 @@ class PayTypeSelector extends React.Component {
           <select id="order_pay_type" onChange={this.onPayTypeSelected} name="order[pay_type]">
             <option value="">Select a payment method</option>
             <option value="Check">Check</option>
-            <option value="Credit card">Credit Card</option>
+            <option value="Credit card">Credit card</option>
             <option value="Purchase order">Purchase order</option>
           </select>
         </div>
